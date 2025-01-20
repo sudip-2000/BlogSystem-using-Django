854b424b4d7e718a5d6c152f591750e4068b078e
sudip-2000 <sparajuli802@gmail.com>
2025-01-20 17:45:14 +0545

    added users profile

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -137,3 +137,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
 LOGIN_REDIRECT_URL='blog-home'
+LOGIN_URL = 'login'
--- blog_system/urls.py
+++ blog_system/urls.py
@@ -23,7 +23,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html', redirect_authenticated_user=True), name='login'),
+
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 
 ]
